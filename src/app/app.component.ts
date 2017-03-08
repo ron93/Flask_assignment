450acac99a2affa12ee7b4aa954b0ef45ec28e26
ron93 <ronmkare1@gmail.com>
2017-03-08 12:31:29 +0300

    http injection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http , Response } from  '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -7,10 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Welcome to GiphySearch';
+  link = 'http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=';
+  http: Http;
+  
+ constructor(http:Http){
+      this.http = http;
+  } 
 
-  performSearch(searchTerm : HTMLInputElement): void{
-    console.log(`user entered : ${searchTerm.value}`);
+  performSearch(searchTerm : HTMLInputElement): void {
+      var apiLink = this.link + searchTerm.value;
+      
+      this.http.request(apiLink)
+          .subscribe((res: Response) =>{
+            console.log(res.json());
+          });
     }
-    
-}
+  
+};
 
